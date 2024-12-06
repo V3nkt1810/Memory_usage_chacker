22d7e38bc7e7b572a4e5cb32aebdbac08597c029
v3nkt1810 <venkatsv1252@gmail.com>
2024-12-06 20:40:10 +0530

    modified:   memory.sh This code has been updated from while to for loop

--- memory.sh
+++ memory.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
-
+ 
 if [ -z "$1" ]; then
     echo "Usage: $0 vuser.txt"
     exit 1
 fi
-
+ 
 server_list=$1
-
+ 
 if [ ! -f "$server_list" ]; then
     echo "File $server_list not found!"
     exit 1
 fi
-
-
+ 
 print_table() {
     printf "%-10s %-20s %-10s %-8s %-s\n" "PID" "USER" "MEM(%)" "COMMAND"
     printf "%-10s %-20s %-10s %-8s %-s\n" "--------" "--------------------" "--------" "-------------------------"
 }
-
-while IFS= read -r server_name; do
+ 
+for server_name in $(cat "$server_list"); do
+    echo "\n---------------------------------------------"
+    echo "Memory utilization on $server_name Server"
     echo "---------------------------------------------"
-    echo "Checking Memory utilization on $server_name"
-    echo "---------------------------------------------"
     
-    ssh -q $server_name '
-    print_table() {
-        printf "%-10s %-20s %-10s %-8s %-s\n" "PID" "USER" "MEM(%)" "COMMAND"
-        printf "%-10s %-20s %-10s %-8s %-s\n" "--------" "--------------------" "--------" "-------------------------"
-    }
-    
-    print_table
-    
-    ps -eo pid,user,%mem,comm --sort=-%mem | head -6 | tail -5 | while read pid user mem cmd; do
-        printf "%-10s %-20s %-10s %-8s %-s\n" "$pid" "$user" "$mem" "$cmd"
-    done
+    ssh -q "$server_name" '
+        print_table() {
+            printf "%-10s %-20s %-10s %-8s %-s\n" "PID" "USER" "MEM(%)" "COMMAND"
+            printf "%-10s %-20s %-10s %-8s %-s\n" "--------" "--------------------" "--------" "-------------------------"
+        }
+        print_table
+        ps -eo pid,user,%mem,comm --sort=-%mem | head -6 | tail -5 | while read pid user mem cmd; do
+            printf "%-10s %-20s %-10s %-8s %-s\n" "$pid" "$user" "$mem" "$cmd"
+        done
     '
-    echo End "\n"
-done < "$server_list"
+    
+    echo "\nPlease find the above Top 5 Utilization on $server_name\n"
+done
